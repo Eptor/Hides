ef845aafdd4cd8b3f0223300950a2ab1ad4be168
Eptor <hector_espinoza02@hotmail.com>
2020-07-11 08:41:51 -0700

    Designer_Files and Windows now match

--- Windows/encrypt_menu.py
+++ Windows/encrypt_menu.py
@@ -9,26 +9,27 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-menu_icon = "Icons/favicon.ico"
-search_folder_icon = "Icons/folder-search.png"
-original_img_icon = "Icons/image-file-search.png"
-terminal_icon = "Icons/terminal.png"
-
 
 class Ui_encrypt_menu(object):
     def setupUi(self, encrypt_menu):
         encrypt_menu.setObjectName("encrypt_menu")
         encrypt_menu.setEnabled(True)
         encrypt_menu.resize(665, 271)
-        encrypt_menu.setWindowIcon(QtGui.QIcon(menu_icon))
         self.centralwidget = QtWidgets.QWidget(encrypt_menu)
         self.centralwidget.setObjectName("centralwidget")
         self.files_list = QtWidgets.QListWidget(self.centralwidget)
         self.files_list.setGeometry(QtCore.QRect(10, 10, 321, 211))
         self.files_list.setObjectName("files_list")
         self.search_folder = QtWidgets.QPushButton(self.centralwidget)
-        self.search_folder.setIcon(QtGui.QIcon(search_folder_icon))
         self.search_folder.setGeometry(QtCore.QRect(10, 230, 321, 31))
+        self.search_folder.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(
+            QtGui.QPixmap("Designer_Files\\../Icons/folder-search.png"),
+            QtGui.QIcon.Normal,
+            QtGui.QIcon.Off,
+        )
+        self.search_folder.setIcon(icon)
         self.search_folder.setObjectName("search_folder")
         self.original_img = QtWidgets.QLineEdit(self.centralwidget)
         self.original_img.setEnabled(True)
@@ -39,29 +40,47 @@ class Ui_encrypt_menu(object):
         self.original_img_label.setGeometry(QtCore.QRect(350, 10, 81, 16))
         self.original_img_label.setObjectName("original_img_label")
         self.search_image = QtWidgets.QPushButton(self.centralwidget)
-        self.search_image.setIcon(QtGui.QIcon(original_img_icon))
         self.search_image.setGeometry(QtCore.QRect(580, 30, 75, 23))
+        self.search_image.setText("")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(
+            QtGui.QPixmap("Designer_Files\\../Icons/image-file-search.png"),
+            QtGui.QIcon.Normal,
+            QtGui.QIcon.Off,
+        )
+        self.search_image.setIcon(icon1)
         self.search_image.setObjectName("search_image")
         self.new_name_label = QtWidgets.QLabel(self.centralwidget)
         self.new_name_label.setGeometry(QtCore.QRect(350, 150, 111, 16))
         self.new_name_label.setObjectName("new_name_label")
         self.new_name = QtWidgets.QLineEdit(self.centralwidget)
+        self.new_name.setEnabled(True)
         self.new_name.setGeometry(QtCore.QRect(350, 170, 301, 21))
+        self.new_name.setReadOnly(False)
         self.new_name.setObjectName("new_name")
         self.start = QtWidgets.QPushButton(self.centralwidget)
-        self.start.setIcon(QtGui.QIcon(terminal_icon))
         self.start.setGeometry(QtCore.QRect(350, 200, 301, 61))
+        self.start.setText("")
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(
+            QtGui.QPixmap("Designer_Files\\../Icons/terminal.png"),
+            QtGui.QIcon.Normal,
+            QtGui.QIcon.Off,
+        )
+        self.start.setIcon(icon2)
         self.start.setObjectName("start")
         self.new_location_label = QtWidgets.QLabel(self.centralwidget)
-        self.new_location_label.setGeometry(QtCore.QRect(350, 80, 101, 16))
+        self.new_location_label.setGeometry(QtCore.QRect(350, 70, 101, 16))
         self.new_location_label.setObjectName("new_location_label")
         self.new_location = QtWidgets.QLineEdit(self.centralwidget)
-        self.new_location.setReadOnly(True)
+        self.new_location.setEnabled(True)
         self.new_location.setGeometry(QtCore.QRect(350, 100, 221, 21))
+        self.new_location.setReadOnly(True)
         self.new_location.setObjectName("new_location")
         self.search_location = QtWidgets.QPushButton(self.centralwidget)
-        self.search_location.setIcon(QtGui.QIcon(search_folder_icon))
         self.search_location.setGeometry(QtCore.QRect(580, 100, 75, 23))
+        self.search_location.setText("")
+        self.search_location.setIcon(icon)
         self.search_location.setObjectName("search_location")
         encrypt_menu.setCentralWidget(self.centralwidget)
 
@@ -73,9 +92,12 @@ class Ui_encrypt_menu(object):
         encrypt_menu.setWindowTitle(
             _translate("encrypt_menu", "Hides - The files hider")
         )
-        # self.search_folder.setText(_translate("encrypt_menu", "Search"))
-        self.original_img_label.setText(_translate("encrypt_menu", "Original photo"))
-        self.new_name_label.setText(_translate("encrypt_menu", "Name for the new file"))
+        self.original_img_label.setText(
+            _translate("encrypt_menu", "Original photo")
+        )
+        self.new_name_label.setText(
+            _translate("encrypt_menu", "Name for the new file")
+        )
         self.new_location_label.setText(
             _translate("encrypt_menu", "Location for new file")
         )
